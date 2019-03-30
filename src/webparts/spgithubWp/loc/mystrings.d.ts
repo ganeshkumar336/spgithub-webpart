1b74ef66a952f352dc697513c5ff2560bc9f67ce
ganeshkumar336 <ganesh.kumar336@gmail.com>
2019-03-30 09:18:57 +0530

    Initial Commit for webpart creation AB#1

---
+++ src/webparts/spgithubWp/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ISpgithubWpWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'SpgithubWpWebPartStrings' {
+  const strings: ISpgithubWpWebPartStrings;
+  export = strings;
+}
